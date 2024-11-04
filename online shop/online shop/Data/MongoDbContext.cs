a29c2bf3d592abd7b484b59c04251648cbf1edbd
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-11-04 21:18:26 +0330

    add MongoDb configuration

---
+++ online shop/online shop/Data/MongoDbContext.cs
@@ -0,0 +1,16 @@
+using MongoDB.Driver;
+
+namespace online_shop.Data;
+
+public class MongoDbContext
+{
+    private readonly IMongoDatabase _database;
+
+    public MongoDbContext(IConfiguration configuration)
+    {
+        var client = new MongoClient(configuration.GetConnectionString("MongoDB"));
+        _database = client.GetDatabase("OnlineShop");
+    }i
+
+    // public IMongoCollection<User> Users => _database.GetCollection<User>("Users");
+}
