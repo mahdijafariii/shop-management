d4f0b4671ed57eef6c43e03744399bf8169bf93b
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-11-10 15:15:23 +0330

    add validator for add seller

---
+++ online shop/online shop/Validator/AddSellerValidator.cs
@@ -0,0 +1,32 @@
+using FluentValidation;
+using Newtonsoft.Json;
+using online_shop.DTO;
+
+namespace online_shop.Validator;
+
+public class AddSellerValidator : AbstractValidator<AddSellerDto>
+{
+    public AddSellerValidator()
+    {
+        RuleFor(x => x.Name)
+            .NotEmpty().WithMessage("Name is required.")
+            .Length(2, 100).WithMessage("Name must be between 2 and 100 characters.");
+
+        RuleFor(x => x.phone)
+            .NotEmpty().WithMessage("Phone is required.");
+
+        RuleFor(x => x.CityId)
+            .GreaterThan(0).WithMessage("CityId must be a positive integer.")
+            .Must(BeAValidCityId).WithMessage("CityId does not exist.");
+    }
+
+
+    private bool BeAValidCityId(int cityId)
+    {
+        var filePath = Path.Combine(Directory.GetCurrentDirectory(), "../online shop/cities/provinces.json");
+        var jsonContent = File.ReadAllText(filePath);
+        var cities = JsonConvert.DeserializeObject<List<City>>(jsonContent);
+        return cities.Any(city => city.id == cityId);
+    }
+}
+
