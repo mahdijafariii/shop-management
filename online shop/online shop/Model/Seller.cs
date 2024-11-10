4103bb2fdde159cb1d0c63825d35bb00129a1d2b
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-11-10 14:28:06 +0330

    feat seller model

---
+++ online shop/online shop/Model/Seller.cs
@@ -0,0 +1,37 @@
+using MongoDB.Bson;
+using MongoDB.Bson.Serialization.Attributes;
+
+namespace online_shop.Model;
+
+public class Seller
+{
+    [BsonId]
+    public ObjectId Id { get; set; }
+
+    [BsonRequired]
+    [BsonElement("name")]
+    public string Name { get; set; }
+
+    [BsonElement("contactDetails")]
+    public ContactDetails ContactDetails { get; set; }
+
+    [BsonElement("cityId")]
+    [BsonRequired]
+    public int CityId { get; set; }
+
+    [BsonElement("user")]
+    [BsonRequired]
+    public ObjectId User { get; set; }
+
+    [BsonElement("createdAt")]
+    public DateTime CreatedAt { get; set; }
+
+    [BsonElement("updatedAt")]
+    public DateTime UpdatedAt { get; set; }
+}
+public class ContactDetails
+{
+    [BsonElement("phone")]
+    [BsonRequired]
+    public string Phone { get; set; }
+}
