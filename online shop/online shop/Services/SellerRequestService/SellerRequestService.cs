9d19d5db01db875135eb1be5b1f5cc080fac124e
Mahdi Jafari <mahdijm.bb@gmail.com>
2024-12-19 18:42:15 +0330

    check product exist before send request

--- online shop/online shop/Services/SellerRequestService/SellerRequestService.cs
+++ online shop/online shop/Services/SellerRequestService/SellerRequestService.cs
@@ -1,5 +1,7 @@
 using online_shop.DTO;
+using online_shop.Exception;
 using online_shop.Model;
+using online_shop.Repositories.ProductRepository;
 using online_shop.Repositories.SellerRequestRepository;
 
 namespace online_shop.Services;
@@ -7,14 +9,22 @@ namespace online_shop.Services;
 public class SellerRequestService : ISellerRequestService
 {
     private readonly ISellerRequestRepository _sellerRequestRepository;
+    private readonly IProductRepository _productRepository;
 
-    public SellerRequestService(ISellerRequestRepository sellerRequestRepository)
+
+    public SellerRequestService(ISellerRequestRepository sellerRequestRepository, IProductRepository productRepository)
     {
         _sellerRequestRepository = sellerRequestRepository;
+        _productRepository = productRepository;
     }
 
     public async Task<string> CreateRequest(string sellerId, CreateSellerRequest request)
     {
+        var product = await _productRepository.IsProductExist(request.ProductId);
+        if (product is null)
+        {
+            throw new NotFoundException("Product");
+        }
         var sellerRequest = new SellerRequest()
         {
             ProductId = request.ProductId,
